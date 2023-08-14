5cb17ec8301d4e974ef9616b418abbed33462eb7
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-14 11:05:09 -0600

    poc

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 metodos del array
 
 # pusch()
-``` El método push() de JavaScript agrega elementos al final de un array. El método push() tiene un solo parámetro, que es el elemento o los elementos que se agregarán al array.
+``` El método push() de JavaScript agrega elementos al final de un array. El método push() tiene un solo parámetro, que es el elemento o los elementos que se agregarán al array.```
+
+# pop():
+`El método pop() de JavaScript elimina el último elemento de un array y devuelve el elemento eliminado. El método pop() no tiene parámetros.`
---
+++ pop.js
@@ -0,0 +1,28 @@
+// metodo 1
+const arreglo1 = [1,2,3,4,5]
+console.log(arreglo1);
+arreglo1.pop();
+console.log(arreglo1);
+
+// metodo 2
+const arreglo2 = [7,8,9,10];
+console.log(arreglo2);
+const lastElement = arreglo2.pop();
+console.log(arreglo2); 
+
+// metodo 2
+const arreglo3 = ["Felicia", "Florencia", "Gaspar"]
+console.log(arreglo3);
+const final = arreglo3.pop();
+console.log(arreglo3);
+
+// metodo 1
+const numeros = [10,20,30,40,50,60]
+console.log(numeros);
+numeros.pop();
+console.log(numeros);
+
+const departamentos = ["sonsonate", "santa ana", "ahuchapan", "san salvador", "la union"]
+console.log(departamentos);
+departamentos.pop()
+console.log(departamentos);
