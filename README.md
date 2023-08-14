98d6d2cfe23ed3bf021c280f7adfac88ba4a71b5
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-14 10:06:02 -0600

    push

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # arrayMethods
 metodos del array
+
+# pusch()
+``` El método push() de JavaScript agrega elementos al final de un array. El método push() tiene un solo parámetro, que es el elemento o los elementos que se agregarán al array.
---
+++ push.js
@@ -0,0 +1,23 @@
+
+const arreglo = [1,2,3]
+console.log(arreglo);
+arreglo.push(4,5)
+console.log(arreglo);
+
+
+const arreglo2 = ["Roberto", "Carlos"]
+console.log(arreglo2);
+arreglo2.push("Salama", "Hernandez")
+console.log(arreglo2);
+
+
+const arreglo3 = [2, 4]
+console.log(arreglo3);
+arreglo3.push(6, 8)
+console.log(arreglo3);
+
+const arreglo4 = ["sonzacate", "san antonio"]
+console.log(arreglo4);
+arreglo4.push("sosonate")
+console.log(arreglo4);
+
