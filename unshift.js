79f9fa7140ca9db057e608f1ebacf3136d3767cb
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-15 09:56:52 -0600

    unShift

---
+++ unshift.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,4,5]
+console.log(arr);
+arr.unshift(0);
+console.log(arr);
+
+// departamentos
+const departamentos = ["sonsonate", "santa ana", "san salvador"]
+console.log(departamentos);
+departamentos.unshift("la union");
+console.log(departamentos);
+
+// peliculas
+const peliculas = ["spider man", "barbie", "super man"]
+console.log(peliculas);
+peliculas.unshift("los vengadores")
+console.log(peliculas);
