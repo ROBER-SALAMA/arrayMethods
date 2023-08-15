f9f276a50368a683caebf84e3dc1c3b5a9098fd1
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-15 09:12:20 -0600

    shift

---
+++ shift.js
@@ -0,0 +1,39 @@
+// metodo 1
+const arr = [1,2,3,4,5];
+console.log(arr);
+arr.shift()
+console.log(arr);
+
+// metodo2
+const arr1 = [1,2,3,4,5,6,7,]
+const arrFinal = arr1.shift();
+console.log(arr1);
+console.log(arrFinal);
+
+// nombre
+const arr2 = ["roberto", "carlos", "salama", "hernandez"]
+console.log(arr2);
+const arrFinal2 = arr2.shift();
+console.log(arr2);
+console.log(arrFinal2);
+
+// departamentos
+const arr3 = ["sonsonate", "santa ana", "ahuchapan", "san salvador", "la union"]
+console.log(arr3);
+const arr3Final = arr3.shift();
+console.log(arr3);
+console.log(arr3Final);
+
+// municipios
+const municipios = ["sozacate", "sonsonate", "san antonio", "acajula"]
+console.log(municipios);
+const municipiosFinal = municipios.shift();
+console.log(municipios);
+console.log(municipiosFinal);
+
+// peliculas
+const peliculas = ["barbie", "batman", "super man", "spider man"]
+console.log(peliculas);
+const peliculasFinal = peliculas.shift();
+console.log(peliculas);
+console.log(peliculasFinal);
