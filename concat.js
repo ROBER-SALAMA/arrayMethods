196976389dc092b8dee1e5e1b97ecf98fe5f50d0
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-15 10:16:41 -0600

    concat

---
+++ concat.js
@@ -0,0 +1,21 @@
+// numeros
+const num = [1,2,3,4,5]
+const num2 = [6,7,8,9,10]
+const newNum = num.concat(num2)
+console.log(newNum);
+
+console.log("----------------------------------");
+
+// nombre 
+const nombre = ["roberto", "carlos"]
+const apellido = ["salama", "hernandez"]
+const newConcat = nombre.concat(apellido)
+console.log(newConcat);
+
+console.log("----------------------------------");
+
+// peliculas
+const peliculas1 = ["bat man", "super man"]
+const peliculas2 = ["capitan america", "los vengadores"]
+const newPelicula = peliculas1.concat(peliculas2)
+console.log(newPelicula);
